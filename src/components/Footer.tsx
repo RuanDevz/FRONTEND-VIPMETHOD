df0de1d5a94cc2d0cc5fa9a25d6248ad2cb0d40e
RuanDevz <Ruanbatista1509@outlook.com>
2024-10-11 19:22:10 -0300

    fix: fix API

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8 fixed inset-x-0 bottom-0">
+    <footer className="bg-gray-800 text-white py-2 fixed inset-x-0 bottom-0">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
         <div className="mb-4 md:mb-0 text-center md:text-left">
           <h2 className="text-2xl font-bold">VIP Method</h2>
--- src/components/HeaderLogged/HeaderButton.tsx
+++ src/components/HeaderLogged/HeaderButton.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 const HeaderButton: React.FC = () => {
   return (
+
+    //
     <Link to="/plans">
       <button className="py-3 px-6 bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 text-black font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
         Buy VIP Access
